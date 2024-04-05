b5919566212a30387c8f29d148ffc53f0c5b618a
MarinPichon <marin.pichon@oclock.school>
2024-04-05 11:04:17 +0200

    Commit

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
 // ton code ici
+const grille = document.createElement('div');
+grille.className = 'grille';
+
+const body = document.querySelector('body');
+body.append(grille);
+
+function generationGrille(userChoice) {
+
+    for (let i = 0 ; i < userChoice; i++){
+        const row = document.createElement('div');
+        grille.append(row);
+        
+    for (let j =0 ; j < userChoice; j++) {
+        const colonne = document.createElement('div');
+        row.append(colonne);
+        }
+    }
+}
+
+
